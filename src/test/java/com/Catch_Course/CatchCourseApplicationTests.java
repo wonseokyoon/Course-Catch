d243146b8f50e74fa4211bba85cb3732884d4fe4
wonseokyoon <01088115030a@gmail.com>
2025-07-16 20:34:04 +0900

    init: 개발 초기 설정 세팅

---
+++ src/test/java/com/Catch_Course/CatchCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Catch_Course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatchCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
