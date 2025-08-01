2396bc0c97ea7a36a29d50ba81d062d52896e402
nokkae <01088115030a@gmail.com>
2025-08-01 21:11:22 +0900

    chore: 디렉토리 구조 변경

--- README.md
+++
@@ -1,2 +0,0 @@
-# Course-Catch
-수강신청 서비스 "콜캐치(Course-Catch)"
--- src/main/java/com/Catch_Course/global/app/AppConfig.java
+++
@@ -1,9 +0,0 @@
-package com.Catch_Course.global.app;
-
-public class AppConfig {
-
-    public static boolean isNotProd() {
-        return true;
-    }
-
-}
