d8c6e39449bb099443bc69b5d863740f6464a5d4
wonseokyoon <backend.wonseok@gmail.com>
2025-08-04 17:07:10 +0900

    feat: 리다이렉트 설정

--- backend/src/main/java/com/Catch_Course/global/security/SecurityConfig.java
+++ backend/src/main/java/com/Catch_Course/global/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.Catch_Course.global.security;
 
 import com.Catch_Course.global.dto.RsData;
 import com.Catch_Course.global.util.Ut;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -46,7 +47,13 @@ public class SecurityConfig {
                 .cors(cors -> {})
                 .oauth2Login(oauth2->{
                     oauth2.successHandler((request, response, authentication) -> {
-                        response.sendRedirect("http://localhost:3000");
+                        HttpSession session = request.getSession();
+                        String redirectUrl = (String) session.getAttribute("redirectUrl");  // 세션 객체를 가져옴(로그인 전 방문 한 주소)
+                        if(redirectUrl == null) {   // 바로 로그인 한 경우
+                            redirectUrl ="http://localhost:3000";   // 리다이렉트 주소를 홈으로 설정
+                        }
+                        session.removeAttribute("redirectUrl"); // 기존 세션 주소 삭제(리디렉션 정보 재사용 방지)
+                        response.sendRedirect(redirectUrl);
                     });
                 })
                 .addFilterBefore(customAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
