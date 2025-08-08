bb600d624ef7cdf87220733a9bc102eb2a8c5821
wonseokyoon <backend.wonseok@gmail.com>
2025-08-08 19:30:56 +0900

    refactor: 신청 내역 완전 하드삭제 하는 대신 브로커에 삭제 이력 관리

--- backend/src/main/java/com/Catch_Course/domain/reservation/service/ReservationService.java
+++ backend/src/main/java/com/Catch_Course/domain/reservation/service/ReservationService.java
@@ -4,12 +4,14 @@ import com.Catch_Course.domain.course.entity.Course;
 import com.Catch_Course.domain.course.repository.CourseRepository;
 import com.Catch_Course.domain.member.entity.Member;
 import com.Catch_Course.domain.member.repository.MemberRepository;
-import com.Catch_Course.global.kafka.producer.ReservationProducer;
-import com.Catch_Course.global.kafka.dto.ReservationRequest;
 import com.Catch_Course.domain.reservation.entity.Reservation;
 import com.Catch_Course.domain.reservation.entity.ReservationStatus;
 import com.Catch_Course.domain.reservation.repository.ReservationRepository;
 import com.Catch_Course.global.exception.ServiceException;
+import com.Catch_Course.global.kafka.dto.ReservationDeletedRequest;
+import com.Catch_Course.global.kafka.dto.ReservationRequest;
+import com.Catch_Course.global.kafka.producer.ReservationDeletedProducer;
+import com.Catch_Course.global.kafka.producer.ReservationProducer;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -28,6 +30,7 @@ public class ReservationService {
     private final ReservationRepository reservationRepository;
     private final MemberRepository memberRepository;
     private final ReservationProducer reservationProducer;
+    private final ReservationDeletedProducer reservationDeletedProducer;
 
     public Reservation addToQueue(Member member, Long courseId) {
         Course course = courseRepository.findByIdWithPessimisticLock(courseId)  // 비관적 Lock 을 걸고 조회
@@ -35,15 +38,23 @@ public class ReservationService {
 
         Optional<Reservation> optionalReservation = reservationRepository.findByStudentAndCourse(member, course);
         if (optionalReservation.isPresent()) {
-            handleExistingReservation(optionalReservation.get(), course);
-            return optionalReservation.get();
+
+            // 중복 신청
+            if(!isCanceled(optionalReservation.get())){
+                handleDuplicateReservation(optionalReservation.get(),member.getId(),courseId);
+                return optionalReservation.get();
+            }
+
+            // 삭제
+            reservationRepository.delete(optionalReservation.get());
         }
+
         // 대기열 등록
         Reservation reservation = Reservation.builder()
-                .student(member)
-                .course(course)
-                .status(ReservationStatus.WAITING)
-                .build();
+                        .student(member)
+                        .course(course)
+                        .status(ReservationStatus.WAITING)
+                        .build();
 
         // 메세지 전송
         reservationProducer.send(new ReservationRequest(member.getId(), courseId));
@@ -51,42 +62,19 @@ public class ReservationService {
         return reservationRepository.save(reservation);
     }
 
-//    public Reservation reserve(Member member, Long courseId) {
-//
-//        Course course = courseRepository.findByIdWithPessimisticLock(courseId)  // 비관적 Lock 을 걸고 조회
-//                .orElseThrow(() -> new ServiceException("404-1", "존재하지 않는 강의입니다."));
-//
-//        Optional<Reservation> optionalReservation = reservationRepository.findByStudentAndCourse(member, course);
-//
-//        if (optionalReservation.isPresent()) {
-//            // 신청 내역이 존재하는 경우 처리 메서드
-//            return handleExistingReservation(optionalReservation.get(), course);
-//        }
-//
-//        course.increaseReservation();
-//        courseRepository.save(course);
-//
-//        Reservation reservation = Reservation.builder()
-//                .student(member)
-//                .course(course)
-//                .status(ReservationStatus.COMPLETED)
-//                .build();
-//
-//        return reservationRepository.save(reservation);
-//    }
-
-    private void handleExistingReservation(Reservation reservation, Course course) {
+    private boolean isCanceled(Reservation reservation) {
         ReservationStatus status = reservation.getStatus();
+        return status == ReservationStatus.CANCELED;
+    }
+
+    private void handleDuplicateReservation(Reservation reservation,Long memberId,Long courseId) {
+        ReservationStatus status = reservation.getStatus();
+
         if (status.equals(ReservationStatus.COMPLETED)) {
             throw new ServiceException("409-1", "이미 신청한 강의입니다.");
         } else if (status.equals(ReservationStatus.WAITING)) {
             throw new ServiceException("409-3", "이미 대기열에 등록된 신청입니다.");
         }
-
-//        course.increaseReservation();
-//        courseRepository.save(course);
-//        reservation.setStatus(ReservationStatus.COMPLETED);   // 상태 변경
-//        return reservationRepository.save(reservation);
     }
 
     public Reservation cancelReserve(Member member, Long courseId) {
@@ -107,6 +95,8 @@ public class ReservationService {
         reservation.setStatus(ReservationStatus.CANCELED);    // 상태 관리로 삭제 처리
 //        reservationRepository.delete(reservation);        // 나중에 필요하면 삭제 처리
 
+        // 수강 취소는 메세지를 남겨서 저장
+        reservationDeletedProducer.send(new ReservationDeletedRequest(member.getId(), courseId));
         return reservationRepository.save(reservation);
     }
 
