c2b83df84d36d8efc4a2d5274a4b27dd65202aa4
wonseokyoon <backend.wonseok@gmail.com>
2025-08-13 16:50:47 +0900

    fix: Lazy예외 처리

--- backend/src/main/java/com/Catch_Course/domain/course/repository/CourseRepository.java
+++ backend/src/main/java/com/Catch_Course/domain/course/repository/CourseRepository.java
@@ -4,6 +4,7 @@ import com.Catch_Course.domain.course.entity.Course;
 import jakarta.persistence.LockModeType;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
@@ -13,10 +14,13 @@ import java.util.Optional;
 
 public interface CourseRepository extends JpaRepository<Course, Long> {
     // 강사로 검색
+    @EntityGraph(attributePaths = {"instructor"})
     Page<Course> findAllByInstructor_Nickname(String keyword, Pageable pageable);
     // 내용으로 검색
+    @EntityGraph(attributePaths = {"instructor"})
     Page<Course> findAllByContentContaining(String keyword, Pageable pageable);
     // 제목으로 검색
+    @EntityGraph(attributePaths = {"instructor"})
     Page<Course> findAllByTitleContaining(String keyword, Pageable pageable);
 
     @Lock(LockModeType.PESSIMISTIC_WRITE)   // 비관적 Lock, FOR UPDATE 전부 차단
